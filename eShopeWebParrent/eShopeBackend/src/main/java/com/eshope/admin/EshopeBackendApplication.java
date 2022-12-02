056a3a02ef6d83102c78148e07eae4b84ceb7f4c
trjindal <trjindal00@gmail.com>
2022-12-02 22:27:41 +0530

    Made multilevel module for eShope

---
+++ eShopeWebParrent/eShopeBackend/src/main/java/com/eshope/admin/EshopeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.eshope.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopeBackendApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(EshopeBackendApplication.class,args);
+    }
+}
---
+++ eShopeWebParrent/eShopeFrontend/src/main/java/com/eshope/site/EshopeFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.eshope.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopeFrontendApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(EshopeFrontendApplication.class,args);
+    }
+}
