6033fa04de4af8b6eb859f3e3bcc24fb8e2584fe
trjindal <trjindal00@gmail.com>
2022-12-11 12:52:51 +0530

    ADD Pagination And sorting+ delete modal working properly

--- eShopeWebParrent/eShopeBackend/src/main/java/com/eshope/admin/Controller/UserController.java
+++ eShopeWebParrent/eShopeBackend/src/main/java/com/eshope/admin/Controller/UserController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -34,12 +35,12 @@ public class UserController {
 
     @GetMapping("/users")
     public String listAllUsers(Model model){
-        return listByPage(1,model);
+        return listByPage(1,model,"id","asc");
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name="pageNum") int pageNum,Model model){
-        Page<User> page=userService.listByPage(pageNum);
+    public String listByPage(@PathVariable(name="pageNum") int pageNum, Model model, @Param("sortField") String sortField,@Param("sortDir") String sortDir){
+        Page<User> page=userService.listByPage(pageNum,sortField,sortDir);
         List<User> listUsers=page.getContent();
 
         long startCount =(pageNum-1)*UserService.USERS_PER_PAGE+1;
@@ -54,6 +55,9 @@ public class UserController {
         model.addAttribute("endCount",endCount);
         model.addAttribute("totalItems",page.getTotalElements());
         model.addAttribute("listAllUsers",listUsers);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
         return "users";
     }
 
