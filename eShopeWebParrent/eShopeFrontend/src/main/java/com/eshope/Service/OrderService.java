a4d08cc60eda81d02a91c64069d6aaf36d156c8b
trjindal <trjindal00@gmail.com>
2023-03-23 16:47:33 +0530

    Made Return Order feature for Customer in Frontend UI

--- eShopeWebParrent/eShopeFrontend/src/main/java/com/eshope/Service/OrderService.java
+++ eShopeWebParrent/eShopeFrontend/src/main/java/com/eshope/Service/OrderService.java
@@ -1,12 +1,10 @@
 package com.eshope.Service;
 
 import com.eShope.common.entity.*;
-import com.eShope.common.entity.Order.Order;
-import com.eShope.common.entity.Order.OrderDetail;
-import com.eShope.common.entity.Order.OrderStatus;
-import com.eShope.common.entity.Order.PaymentMethod;
+import com.eShope.common.entity.Order.*;
 import com.eShope.common.entity.Product.Product;
 import com.eshope.PoJo.CheckoutInfo;
+import com.eshope.PoJo.OrderReturnRequest;
 import com.eshope.Repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -95,6 +93,30 @@ public class OrderService {
         }catch (NoSuchElementException ex){
             throw new UsernameNotFoundException("Could not find any order with Id "+ id);
         }
+    }
+
+    public void setOrderReturnRequested(OrderReturnRequest request,Customer customer){
+        Order order=orderRepository.findByIdAndCustomer(request.getOrderId(), customer);
+        if(order==null){
+            throw new UsernameNotFoundException("Could not find any order with Id "+ request.getOrderId());
+        }
+        if(order.isReturnRequested()) return;
+
+        OrderTrack track=new OrderTrack();
+        track.setOrder(order);
+        track.setUpdatedTime(new Date());
+        track.setStatus(OrderStatus.RETURN_REQUESTED);
+
+        String notes="Reason : "+request.getReason();
+        if(!"".equals(request.getNote())){
+            notes+="."+request.getNote();
+        }
+        track.setNotes(notes);
+
+        order.getOrderTracks().add(track);
+        order.setOrderStatus(OrderStatus.RETURN_REQUESTED);
+
+        orderRepository.save(order);
     }
 
 }
---
+++ eShopeWebParrent/eShopeFrontend/src/test/java/com/eshope/OrderRestControllerTests.java
@@ -0,0 +1,61 @@
+package com.eshope;
+
+
+import com.eshope.PoJo.OrderReturnRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderRestControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithUserDetails("trjindal00@gmail.com")
+    public void testSendOrderReturnRequestFailed() throws Exception {
+        Integer orderId=1111;
+        OrderReturnRequest orderReturnRequest=new OrderReturnRequest(orderId,"","");
+
+        String requestUrl="/orders/return";
+
+        mockMvc.perform(post(requestUrl)
+                .with(csrf())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(orderReturnRequest)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails("trjindal00@gmail.com")
+    public void testSendOrderReturnRequestSuccessful() throws Exception {
+        Integer orderId=5;
+        String reason ="I don't like the product.";
+        String note="Please refund ASAP";
+        OrderReturnRequest orderReturnRequest=new OrderReturnRequest(orderId,reason,note);
+
+        String requestUrl="/orders/return";
+
+        mockMvc.perform(post(requestUrl)
+                        .with(csrf())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(orderReturnRequest)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
